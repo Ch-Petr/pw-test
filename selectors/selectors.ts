4a96ccbb2af78684902d20269f55797b89b940b1
Ch-Petr <chalupapetr91@gmail.com>
2024-12-03 20:16:43 +0100

    03.12

--- selectors/selectors.ts
+++ selectors/selectors.ts
@@ -1,7 +1,19 @@
-const login = {
-    USERNAME_CSS: 'input[placeholder="Username"]', //toto používat !!
-    USERNAME_CSS2: '[placeholder*="Username"]', //hvězdička za placeholder hledá cokoli mezi?
+export const login = {
+    USERNAME_CSS: 'input[placeholder="Username"]', //toto ideálně používat
+    USERNAME_CSS2: '[placeholder*="Username"]', //hvězdička za placeholder hledá cokoli mezi
     USERNAME_XPATH: '//*input[@placeholder="Username"]', //ručně napsané na základe html v DevTools (//* = hledá v celém html)
     USERNAME_XPA: '//*[@id="user-name"]', // přes pravé tl. v DevTools -> Copy -> Copy XPath (//* = hledá v celém html)
-    TT: '/html/body/div[8]/div[2]/div/div/div/div[7]/div/div' // vykopírované z DevTools - není dobré, pouze cesta v html
+    TT: '/html/body/div[8]/div[2]/div/div/div/div[7]/div/div', // vykopírované z DevTools - není dobré, pouze cesta v html
+
+
+    USERNAME_CSS3: 'div[id="modal"] >> input[placeholder="Username"]', //řetězování, >> znamena, ze to pouze v ramci div vyhleda input
+    USERNAME_CSS_XPATH: 'div[id="modal"] >> //*input[@placeholder="Username"]', //kombinace CSS s XPATH
+}
+
+//CVIČENÍ:
+
+export const cviceniLogin = {
+    textfield_Username: '[id="user-name"]', //Jméno
+    textfield_Password: '[id="password"]', //Heslo
+    button_Login: '[id="login-button"]', //CTA "Login"
 }
