78d62dbc4456441483ff6388a33bcaa131364f9d
Ch-Petr <chalupapetr91@gmail.com>
2025-01-07 19:59:06 +0100

    7.1.2025

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,36 +1,55 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const TIMEOUTS = {
+  TEST_TIMEOUT: 90_000,
+  GLOBAL_TIMEOUT: 180_000,
+  ACTION_TIMEOUT: 60_000
+};
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const DEFAULT_VIEWPORT = {
+  width: 1920,
+  height: 1080
+};
+
 export default defineConfig({
+  globalTimeout: TIMEOUTS.GLOBAL_TIMEOUT,
+  timeout: TIMEOUTS.TEST_TIMEOUT,
+  viewport: DEFAULT_VIEWPORT,
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0, //true z CI spustí 2 a false z CI spustí 0
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html']
+    ['list']
+  ],
+
+// --------------------------------------------USE----------------------------------------------------
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
+    testIdAttribute: 'data-testId',
+    viewport: DEFAULT_VIEWPORT,
+    actionTimeout: TIMEOUTS.ACTION_TIMEOUT,
+    navigationTimeout: TIMEOUTS.ACTION_TIMEOUT,
+    video: 'off',
+    headless: true,
+    ignoreHTTPSErrors: true
   },
+
+
+// -------------------------------------------PROJECTS-------------------------------------------------
 
   /* Configure projects for major browsers */
   projects: [
@@ -38,7 +57,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,24 +1,39 @@
-import { test } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 
-test.describe("Příklad Test Setu", () =>{
+test.describe("Příklad Test Suite", () => {
     test("User login", async ({ page }) => {
-        const url = 'https://www.saucedemo.com/'
-        await page.goto(url, {waitUntil: 'load', timeout: 10_000})
+        await test.step("Otevřít stránku", async () => {
+            const url = 'https://www.saucedemo.com'
+            await page.goto(url, { waitUntil: 'load', timeout: 10_000 })
+        })
 
-        const usernameSelector = 'input[data-test="username"]'
-        await page.locator(usernameSelector).first().fill("standard_user", {timeout: 5_000, force: true})
+        await test.step("Vyplnit Username", async () => {
+            const usernameLocator = page.locator('input[data-test="username"]').first()
+            await expect (usernameLocator).toHaveText('standard_user')
+            await expect(usernameLocator).toBeAttached({ timeout: 5_000})
+            await usernameLocator.fill("standard_user", { timeout: 5_000 })
+        })
 
-        const passwordSelector = 'input[data-test="password"]'
-        await page.locator(passwordSelector).first().fill("secret_sauce", {timeout: 5_000, force: true})
+        await test.step("Vyplnit Password", async () => {
+            const  passwordLocator= 'input[id="password"]'
+            await expect (page.locator(passwordLocator).first()).toBeVisible({ timeout: 5_000 })
+            await page.locator(passwordLocator).first().fill("secret_sauce", { timeout: 5_000, force: true })
+        })
 
-        const buttonSelector = 'input[data-test="login-button"]'
-        await page.locator(buttonSelector).first().click() //.click({clickCount: 3}) klikne 3x
+        await test.step("Kliknout na Login button", async () => {
+            const loginButton = 'input[name="login-button"]'
+            await page.locator(loginButton).first().click({ timeout: 5_000, force: true })
+        })
 
+        const loginButton = 'input[name="login-button"]'
+        await page.locator(loginButton).first().click({ timeout: 5_000, force: true })
 
-        await page.waitForLoadState('domcontentloaded', {timeout: 5_000})
+        await page.waitForLoadState('domcontentloaded', { timeout: 5_000 })
 
+        await page.waitForURL(/inventory.html/, { timeout: 5_000, waitUntil: 'domcontentloaded' })
     })
 })
+
 
 
 //spustit: npx playwright test tests/smoke.spec.ts --headed
